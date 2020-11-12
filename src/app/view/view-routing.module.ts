926d9017d53e2d3dc86d5975b496f8945ccd6f3a
Lavdrim <ls26665@seeu.edu.mk>
2020-11-13 00:52:44 +0100

    dumy new components for children routes

--- src/app/view/view-routing.module.ts
+++ src/app/view/view-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeViewComponent } from './home-view/home-view.component';
+import {BiographyComponent } from './biography/biography.component';
+import {BiologyComponent } from './biology/biology.component';
+import {PartnersComponent} from './partners/partners.component';
 
 const routes: Routes = [{
-  path: 'view' ,  component: HomeViewComponent
+  path: 'view' ,  component: HomeViewComponent,
+  children: [
+    {path: '', component: BiographyComponent},
+    {path: 'biology', component: BiologyComponent},
+    {path: 'partners', component: PartnersComponent},
+  ]
 }];
 
 @NgModule({
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ViewRoutingModule } from './view-routing.module';
 import { HomeViewComponent } from './home-view/home-view.component';
 import { TableComponent } from './table/table.component';
+import { BiologyComponent } from './biology/biology.component';
+import { BiographyComponent } from './biography/biography.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
-  declarations: [HomeViewComponent, TableComponent],
+  declarations: [HomeViewComponent, TableComponent, BiologyComponent, BiographyComponent, PartnersComponent],
   imports: [
     CommonModule,
     ViewRoutingModule
